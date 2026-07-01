01411f431cf52deb70b0ba802b1ca196402b3da6
abdrzakk-jpg <abdrzakk.dz@gmail.com>
2026-07-01 08:01:19 +0100

    add /posts route

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/") # "/" => path, .get => method 
 def root():
     return {"msg":"hello api !!!"} 
+
+
+@app.get("/posts")
+def get_posts(req: Request):
+    return {
+        "data": "post 1"
+    }
+    
