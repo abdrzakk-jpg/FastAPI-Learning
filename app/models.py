4b47cb89138e89c821ed93beef5746a216e4e77a
abdrzakk-jpg <abdrzakk.dz@gmail.com>
2026-07-04 15:57:03 +0100

    Add User-model in `models.py`

--- app/models.py
+++ app/models.py
@@ -19,3 +19,14 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, default=True, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('NOW()'), nullable=False)
+
+
+class User(Base):
+    # define table name:
+    __tablename__ = "users"
+    
+    # define columns
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    joined_at = Column(TIMESTAMP(timezone=True), server_default=text('NOW()'), nullable=False)
